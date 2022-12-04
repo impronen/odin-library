685d780921d85023f5c16b5b6fa2c2c0f6e9005d
impronen <impronen@gmail.com>
2022-12-04 18:26:45 +0200

    Setup

--- code.js
+++ code.js
@@ -1,3 +1,9 @@
+//Global variables
+
+let myLibrary = [];
+
+
+
 function book(author, pages, haveRead) {
     this.author = author
     this.pages = pages
@@ -6,18 +12,4 @@ function book(author, pages, haveRead) {
     this.info = function() {
         return (`${author}, ${pages}, ${haveRead}`) //function inside the constructor function
     }
-}
-
-
-function Student(name, grade) {
-  this.name = name
-  this.grade = grade
-  
-}
-
-Student.prototype.sayName = function() {
-  console.log(this.name)
-}
-Student.prototype.goToProm = function() {
-  console.log("Eh.. go to prom?")
 }
