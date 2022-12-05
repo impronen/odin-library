c50ef50c9015d0bbfc2dbec72f520be2c16857c3
impronen <impronen@gmail.com>
2022-12-05 10:32:15 +0200

    Function separation
    Seperated adding books to library from constructor as
    new items would have inherited nested functions.

--- script.js
+++ script.js
@@ -2,29 +2,24 @@
 
 let myLibrary = [];
 
+//Constructor function
 
 function book(author, pages, haveRead) {
     this.author = author
     this.pages = pages
     this.haveRead = haveRead
 
-    this.pushToLibrary  = function() {
-        myLibrary.push(this);
-    }
-    
-    this.pushToLibrary();
-
-    this.info = function() {
+/*     this.info = function() {
         return (`${author}, ${pages}, ${haveRead}`)
-    }
+    } */
     
 }
 
-
-
+function addBookToLibrary(newBook) {
+    myLibrary.push(newBook);
+}
 
 //Library looper 
-
 
 myLibrary.forEach(myLibrary => {
     for (let key in myLibrary) {
@@ -32,7 +27,7 @@ myLibrary.forEach(myLibrary => {
     }
 })
 
-//
 
-// let TheHobbit = new book ('JRR Tolkien', '256','Not read');
-// let LOTR = new book ('JRR Tolkien', '1056','Not read');
+
+let TheHobbit = new book ('JRR Tolkien', '256','Not read');
+let LOTR = new book ('JRR Tolkien', '1056','Not read');
