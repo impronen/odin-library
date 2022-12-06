310816378a98b21895e478899757f13b8e41e67e
impronen <impronen@gmail.com>
2022-12-06 12:30:41 +0200

    Function to add book from form to array

--- script.js
+++ script.js
@@ -2,12 +2,20 @@
 
 let myLibrary = [];
 
+let newBookName = document.querySelector("#Book");
+let newAuthor = document.querySelector("#Author");
+let newGenre = document.querySelector("#Genre");
+
+
+const addBookBtn = document.querySelector("#new-book");
+
+
 //Constructor function
 
-function book(author, pages, haveRead) {
+function book(author, name, genre) {
     this.author = author
-    this.pages = pages
-    this.haveRead = haveRead
+    this.name = name
+    this.genre = genre
 
 /*     this.info = function() {
         return (`${author}, ${pages}, ${haveRead}`)
@@ -15,9 +23,20 @@ function book(author, pages, haveRead) {
     
 }
 
+//Subfunction to push a new book to array
 function addBookToLibrary(newBook) {
     myLibrary.push(newBook);
 }
+
+//Add book from form to the array
+addBookBtn.addEventListener('click', (event) => {
+    console.log(`New book is ${newBookName.value}`)
+    console.log(`New author is ${newAuthor.value}`)
+    console.log(`The genre is ${newGenre.value}`);
+
+   let newBook = new book (`${newAuthor.value}`, `${newBookName.value}`, `${newGenre.value}`);
+   addBookToLibrary(newBook)
+})
 
 //Library looper 
 
